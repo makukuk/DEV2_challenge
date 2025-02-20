419e26cdd722da94a0f5f891351e9bf1e9bdcde1
makukuk <99019435+makukuk@users.noreply.github.com>
2025-02-20 14:56:33 +0100

    dev2 challenge 2
    server opzetten & express.js

---
+++ server/public/js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Ahoy, matey! The treasure be hidden in plain sight. Seek the hidden message in the source/files...");
+    //
+    // !!!!!!! THE CODE FOR THE HIDDEN TREASURE IS gold123 !!!!!!!
+    //npm install ejs express-ejs-layouts
+});
